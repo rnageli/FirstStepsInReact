04580b7163e727edb0ef7fbbb31785366e9a46af
Ravikiran Nageli <ravikiran.nageli@gmail.com>
2018-03-10 17:29:34 -0500

    First component. Import destructure and fixed eslint errors.

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-/*import { AppRegistry } from 'react-native';
-import App from './App';
-
-AppRegistry.registerComponent('albums', () => App);*/
 
 //Import a lib to help create a component
 import React from 'react';
-import ReactNative from 'react-native';
-//import is an ES6 feature.
+import { Text, AppRegistry } from 'react-native';
+//Import destructure
 
 //create a component
-const App = () => {
-     return (
-          <Text>Some text</Text>
-     );
-};
+const App = () => <Text>Some text</Text>;
+//Arrow function implicitly returns when on one liners.
 
 //render it to the screen
-ReactNative.AppRegistry.registerComponent('albums', () => App);
+AppRegistry.registerComponent('albums', () => App);
 // Arrow func without {} implicitly returns App without return keyword
 // albums is the name of the project/app to render the component
 // Second argument is a func that returns the first comp to render to the app
-//Must always register atleast one component in a react app 
+//Must always register atleast one component in a react app
