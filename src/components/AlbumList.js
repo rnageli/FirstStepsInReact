762c10439477327e21f5aa1615a77cd4219c0425
Ravikiran Nageli <ravikiran.nageli@gmail.com>
2018-05-08 02:31:22 -0400

    Refactored from functional to class based components.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-const AlbumList = () => {
-     return (
-          <View>
-               <Text>A list of albums. </Text>
-          </View>
-     );
-};
+class AlbumList extends Component {
+     render() {
+          return (<View>
+               <Text>A list of albums123. </Text>
+          </View>);
+     }
+}
 
 export default AlbumList;
