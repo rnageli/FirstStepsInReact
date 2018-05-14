1a712696e79cae5be4588b5cb66dd178a2545647
Ravikiran Nageli <ravikiran.nageli@gmail.com>
2018-05-14 01:56:39 -0400

    componentWillMount when component is about to load on to the screen.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
+
 
 class AlbumList extends Component {
+
+     componentWillMount() {
+          axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(
+               response => console.log(response)
+          );
+     }
      render() {
           return (<View>
-               <Text>A list of albums123. </Text>
+               <Text>A list of Albums. </Text>
           </View>);
      }
 }
