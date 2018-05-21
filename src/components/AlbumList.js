b7ccb0fb36802625291f8b894bbec2ce0fc11a51
Ravikiran Nageli <ravikiran.nageli@gmail.com>
2018-05-21 00:48:36 -0400

    Fixed Gradle version error.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -5,15 +5,33 @@ import axios from 'axios';
 
 class AlbumList extends Component {
 
+     state = { albums: [] };
      componentWillMount() {
           axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(
-               response => console.log(response)
+               response => {
+                    this.setState({ albums: response.data });
+          });
+     }
+
+     renderAlbums() {
+          return this.state.albums.map(
+               album => <Text>{album.title}</Text>
           );
+          /*
+          Note on map function:
+               It is used on arrays similar to foreach loop in java.
+               it takes in an arrow funtion as argument which returns some data.
+               So for every album map it to the returned data.
+          */
+          //Note use of JS inside jSX tag in {}
      }
      render() {
+          console.log('Data:', this.state);
           return (<View>
                <Text>A list of Albums. </Text>
+               {this.renderAlbums()}
           </View>);
+          //Note use of JS inside jSX tag in {}
      }
 }
 
