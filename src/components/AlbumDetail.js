3d7905cc4e1f8f68a92ee97bf8cc336e7f94e13a
Ravikiran Nageli <ravikiran.nageli@gmail.com>
2018-05-22 18:55:50 -0400

    flexDirection, space-around, styling.

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -6,16 +6,33 @@ import CardSection from './CardSection';
 const AlbumDetail = (props) => {
      return (
           <Card>
-               <CardSection>
-                    <Text>Title: {props.albumData.title}</Text>
+               <CardSection style={styles.cardHeaderStyle}>
+                    <View style={styles.cardHeaderAlbumIconStyle}>
+                         <Text>Title: {props.albumData.title}</Text>
+                    </View>
+                    <View style={styles.cardHeaderTitleViewStyle}>
+                         <Text>Title: {props.albumData.title}</Text>
+                         <Text>Title: {props.albumData.artist}</Text>
+                    </View>
                </CardSection>
-
-               <CardSection>
-                    <Text>Title: {props.albumData.title}</Text>
-               </CardSection>
-
           </Card>
      );
 };
 
 export default AlbumDetail;
+
+const styles = {
+     cardHeaderAlbumIconStyle: {
+          padding: 3
+     },
+     cardHeaderTitleViewStyle: {
+          flexDirection: 'column',
+          justifyContent: 'space-around',
+          padding: 5
+     },
+     cardHeaderStyle: {
+          justifyContent: 'space-around',
+          flexDirection: 'row'
+     }
+
+};
--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 
+//CardSection seperates sections within each album card. 
 const CardSection = (props)  => {
      return (
           <View style={styles.styleCardSection}>{props.children}</View>
