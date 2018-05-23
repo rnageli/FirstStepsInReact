6cf0706aa4881ee3a2d74943c824400c7d7eea3a
Ravikiran Nageli <ravikiran.nageli@gmail.com>
2018-05-22 22:27:52 -0400

    Image visisble. Comments on destructuring of props.

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
-
+//props is destructured to albumData
 const AlbumDetail = ({ albumData }) => {
      const { title, artist, thumbnail_image } = albumData;
+     //Destructuring of albumData
      return (
           <Card>
                <CardSection style={styles.cardHeaderStyle}>
                     <View style={styles.cardHeaderAlbumIconStyle}>
-                         <Image source={{ uri: thumbnail_image }} />
+                         <Image
+                              style={styles.thumbnailStyle}
+                              source={{ uri: thumbnail_image }}
+                         />
                     </View>
                     <View style={styles.cardHeaderTitleViewStyle}>
                          <Text>Title: {title}</Text>
@@ -34,6 +38,11 @@ const styles = {
      cardHeaderStyle: {
           justifyContent: 'space-around',
           flexDirection: 'row'
+     },
+     thumbnailStyle: {
+          height: 50,
+          width: 50
      }
+     //Image is not visible without height and width specified, because of overlap. 
 
 };
